108a328099f499e7b01ded64713f939b79774ef7
Abhay Gangwal <abhay.gangwal@hanu.com>
2023-02-13 14:17:00 +0530

    second commit

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -1,14 +1,24 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet"
-  address_space       = ["192.168.0.0/16"]
-  location            = "West Europe"
+resource "azurerm_network_security_group" "nsg" {
+  name                = "network-security-group"
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
- 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "action-virtual-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "actionsubnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "actionsubnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.nsg.id
+  }
 }
