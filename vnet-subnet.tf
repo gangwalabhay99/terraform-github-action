1023332542517fb64430264862bb4863beec0578
Abhay Gangwal <abhay.gangwal@hanu.com>
2023-02-13 14:59:13 +0530

    ubuntu vm creation

--- vnet-subnet.tf
+++ vnet-subnet.tf
@@ -1,24 +1,13 @@
-resource "azurerm_network_security_group" "nsg" {
-  name                = "network-security-group"
+resource "azurerm_virtual_network" "vnet" {
+  name                = "v-network"
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "action-virtual-network"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-
-  subnet {
-    name           = "actionsubnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "actionsubnet2"
-    address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.nsg.id
-  }
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.rg.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
